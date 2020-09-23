7a69e84d4805eee61aeaf41fead34ddd76aa3d6c
Maksim Litvyakov <litvy-maksim@yandex.ru>
2020-09-23 11:45:46 +0700

    Added HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class HomeController
+ * @package App\Controller
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render("index.html.twig");
+    }
+}
