08db9b8ff662821e912e0d5b4118e83364c093b9
Maksim Litvyakov <litvy-maksim@yandex.ru>
2020-09-26 09:44:21 +0700

    Added Api-platform

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -2,10 +2,21 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
+ * @ApiResource(
+ *     collectionOperations={"get"={"normalization_context"={"groups"="comments:list"}}},
+ *     itemOperations={"get"={"normalization_context"={"groups"="comment:item"}}},
+ *          order={"createdAt"="DESC"},
+ *          paginationEnabled=false
+ * )
+ * @ApiFilter(SearchFilter::class, properties={"conference": "exact"})
  * @ORM\Entity(repositoryClass="App\Repository\CommentRepository")
  */
 class Comment
@@ -15,6 +26,7 @@ class Comment
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"comment:list", "comment:item"})
      */
     private $id;
 
@@ -23,6 +35,7 @@ class Comment
      * @ORM\Column(nullable=true)
      * @Assert\NotBlank(groups={"anonymous"})
      * @Assert\Length(min = 2, groups={"anonymous"})
+     * @Groups({"comment:list", "comment:item"})
      */
     private $author = null;
 
@@ -31,24 +44,28 @@ class Comment
      * @ORM\Column(type="text")
      * @Assert\NotBlank
      * @Assert\Length(min = 5)
+     * @Groups({"comment:list", "comment:item"})
      */
     private $content;
 
     /**
      * @var \DateTimeImmutable
      * @ORM\Column(type="datetime_immutable")
+     * @Groups({"comment:list", "comment:item"})
      */
     private $postedAt;
 
     /**
      * @var Post
      * @ORM\ManyToOne(targetEntity="Post", inversedBy="comments")
+     * @Groups({"comment:list", "comment:item"})
      */
     private $post;
 
     /**
      * @var null|User
      * @ORM\ManyToOne(targetEntity="User", inversedBy="comments")
+     * @Groups({"comment:list", "comment:item"})
      */
     private $user = null;
 
