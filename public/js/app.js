2994f85886aa8256cc0ed5de33ba4913813b2a7d
Maksim Litvyakov <litvy-maksim@yandex.ru>
2020-09-30 21:31:19 +0700

    removed reboot when deleting a comment

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
-async function removeComment(id) {
+async function removeComment(e, id) {
     const res = await fetch(`https://127.0.0.1:8000/api/comments/${id}`, {
-        method: 'DELETE'
-    }).then(() => location.reload());
+        method: 'DELETE'    
+    });
+    e.parentNode.remove();
 }
